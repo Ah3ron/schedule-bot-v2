15173bbe383bc524a635821517cae88674d64d31
Ah3ron <ah3ron@protonmail.com>
2025-02-12 15:25:45 +0300

    Added auto update schedule

--- main.go
+++ main.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"log"
+	"time"
+
 	"schedule-bot/bot"
 	"schedule-bot/config"
 	"schedule-bot/database"
 	"schedule-bot/parser"
+
+	"gorm.io/gorm"
 )
+
+func updateSchedules(db *gorm.DB) error {
+	schedules, err := parser.ParseAllSchedules()
+	if err != nil {
+		log.Println("Ошибка парсинга расписаний:", err)
+		return err
+	}
+	if err := database.SaveSchedules(db, schedules); err != nil {
+		log.Println("Ошибка сохранения расписаний:", err)
+		return err
+	}
+	return nil
+}
 
 func main() {
 	cfg, err := config.LoadConfig()
@@ -19,16 +36,31 @@ func main() {
 		log.Fatal("Ошибка подключения к БД:", err)
 	}
 
-	allSchedules, err := parser.ParseAllSchedules()
-	if err != nil {
-		log.Fatal("Ошибка парсинга расписаний:", err)
+	if err := updateSchedules(db); err != nil {
+		log.Fatal("Первичное обновление расписаний не выполнено:", err)
 	}
-
-	if err := database.SaveSchedules(db, allSchedules); err != nil {
-		log.Fatal("Ошибка сохранения расписаний:", err)
-	}
+	log.Println("Первичное расписание успешно обновлено.")
 
 	go bot.StartBot(cfg.TelegramToken, db)
+
+	ticker := time.NewTicker(30 * time.Minute)
+	quit := make(chan struct{})
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				log.Println("Обновление расписаний...")
+				if err := updateSchedules(db); err != nil {
+					continue
+				}
+				log.Println("Расписание успешно обновлено.")
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
 
 	select {}
 }
